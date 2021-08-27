4645f838087da051b27443c5ba3f91516084a30d
farzane <45811147+farzaneka@users.noreply.github.com>
2021-08-27 13:47:39 +0430

    Added user model and requirements files (#1)
    * Added models and requirements files
    
    * Update models.py
    
    * Edited user table name and remove unused import
    
    Co-authored-by: Mohammad Sheikhian <mohammadsheikhian70@gmail.com>

---
+++ models.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Date, create_engine
+
+
+engine = create_engine("postgres+psycopg2://postgres:postgres@localhost/sqlalchemy_practice")
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    first_name = Column('first_name',String)
+    last_name = Column('last_name', String)
+    birthday = Column('birthday', Date)
+
+
+Base.metadata.create_all(bind=engine)
+
