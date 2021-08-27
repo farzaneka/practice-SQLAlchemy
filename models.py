68b8e0f67488f075a8e5532dcfc1acb5920b8d0d
Mohammad Sheikhian <mohammadsheikhian70@gmail.com>
2021-08-27 12:33:01 +0430

    Update models.py

--- models.py
+++ models.py
@@ -3,9 +3,10 @@ import inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date, create_engine
 
-engine = create_engine("postgres+psycopg2://postgres:postgres@localhost/sqlalchemy_test")
 
+engine = create_engine("postgres+psycopg2://postgres:postgres@localhost/sqlalchemy_test")
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -13,6 +14,7 @@ class User(Base):
     first_name = Column('first_name',String)
     last_name = Column('last_name', String)
     birthday = Column('birthday', Date)
+
 
 Base.metadata.create_all(bind=engine)
 
